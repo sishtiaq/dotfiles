339073145ec2dd4b048d4eb212f407ef744a489b
Samin Ishtiaq <sishtiaq@microsoft.com>
2017-11-01 13:47:06 +0000

    Windows ps profile.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+﻿
+# Load Modules 
+## posh-git
+#. 'C:\Users\sishtiaq\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+## psreadline
+if ($host.Name -eq 'ConsoleHost')
+{
+    Import-Module PSReadLine
+    Set-PSReadLineOption -EditMode Emacs
+}
+
+
+# alias
+Set-Alias to pushd
+Set-Alias fr popd
